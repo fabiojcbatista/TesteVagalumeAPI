71c41cd3e906972d0a41ed6d7cea4a5d09b0b94e
Fabio Jr <fabiojcbweb@gmail.com>
2022-05-06 17:26:23 -0300

    Remoção de imports não usados, testes de pesquisa com falhas

--- src/test/java/com/vagalumeAPI/TestVagalume.java
+++ src/test/java/com/vagalumeAPI/TestVagalume.java
@@ -1,16 +1,11 @@
 package com.vagalumeAPI;
 
 import org.junit.Test;
-
-import io.restassured.*;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.hasSize;
 
 public class TestVagalume {
     private String urlBase = "https://www.vagalume.com.br";
-
+    
     @Test
     public void dadoVagalumeAPI_QuandoRealizarGetArtista_entaoDeveRetornarStatusCode200(){
         given()
@@ -19,6 +14,39 @@ public class TestVagalume {
             .pathParam("artista", "u2")
         .when()
             .get(urlBase+"/{artista}/index.js")
+        .then()
+            .log()
+            .body()
+            .statusCode(200);
+    }
+
+    //@Test //not
+    public void dadoVagalumeAPI_QuandoRealizarGetELimit_entaoDeveRetornarStatusCode200(){
+        given()
+            .log()
+            .all()
+            .pathParam("pesquisa", "Skank")
+            .pathParam("limite", "5")
+        .when()
+            .get(urlBase+"/search.art?q={pesquisa}&limit={limite}")
+        .then()
+            .log()
+            .body()
+            .statusCode(200);
+    }
+    
+
+
+
+    //@Test 
+    public void dadoVagalumeAPI_QuandoRealizarGetMusicaELimit_entaoDeveRetornarStatusCode200(){
+        given()
+            .log()
+            .all()
+            .pathParam("pesquisa", "vamos fugir")
+            .pathParam("limite", "5")
+        .when()
+            .get(urlBase+"/search.excerpt?q={pesquisa}&limit={limite}")
         .then()
             .log()
             .body()
